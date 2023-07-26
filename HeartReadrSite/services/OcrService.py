4a70897fe825763eaffb25905e4f966bb1cfd9c1
Anjie Zhou <anjie.zhou2003@gmail.com>
2023-07-26 09:30:12 -0500

    Commented OcrService.py and removed some shit

--- HeartReadrSite/services/OcrService.py
+++ HeartReadrSite/services/OcrService.py
@@ -9,6 +9,11 @@ import numpy as np
 class OcrService:
 
     def __init__(self, file_name, x_begin, x_end, y_begin, y_end):
+        '''
+        Constructor for OCR Service
+        :param file_name: the name of the video file to be processed
+        :param x_begin, x_end, y_begin, y_end: Coordinates for cropping the OCR Region
+        '''
         self.file_name = file_name
         self.x_begin = x_begin
         self.x_end = x_end
@@ -18,22 +23,53 @@ class OcrService:
 
     @staticmethod
     def extract_numbers(text):
+        '''
+        Extracts all the numbers from a string
+        :param text: string
+        :returns: a list containing numbers
+        '''
         return re.findall(r'\d+', text)
 
     @staticmethod
     def preprocess_frame(frame, x_begin, x_end, y_begin, y_end):
+        '''
+        Preprocesses a frame:
+        - Converts the frame to grayscale
+        - Crops the frame based on the provided coordinates
+        :param frame: The input frame
+        :param x_begin, x_end, y_begin, y_end: Coordinates for cropping
+        :returns: The preprocessed frame (PIL image)
+        '''
+
+        #Converts the frame to grayscale
         gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #Converts the OpenCV format image to a PIL format
         pil_image = Image.fromarray(gray_image)
+
+        #Crops the frame
         pil_image = pil_image.crop((x_begin, y_begin, x_end, y_end))
+
         return pil_image
 
     @staticmethod
-    def save_frame_as_image(pil_image, count, video_name):
-        frame_filename = f'frames/{video_name}_{count}.png'
+    def save_frame_as_image(pil_image, count):
+        '''
+        Saves the given PIL image as an image file
+        :param pil_image: The PIL image to save
+        :param count: Frame count (for filename)
+        :returns: The filename of the saved image
+        '''
+        frame_filename = f'frames/frame_{count}.png'
         pil_image.save(frame_filename, format='PNG')
         return frame_filename
 
     def recognize_numbers(self, image_names):
+        '''
+        Perform OCR recognition on a list of image names
+        :param image_names: List of image filenames
+        :returns: A list of detected numbers for each frame
+        '''
         pipeline = keras_ocr.pipeline.Pipeline()
         images = [keras_ocr.tools.read(url) for url in image_names]
         prediction_groups = pipeline.recognize(images)
@@ -51,6 +87,8 @@ class OcrService:
         return value_per_frame
 
     def process_video(self):
+
+        #Open the video file
         video_cap = cv2.VideoCapture(self.file_name)
 
         if not video_cap.isOpened():
@@ -58,21 +96,29 @@ class OcrService:
 
         image_names = []
         count = 1
+
+        #Iterating through every frame to process
         while video_cap.isOpened():
+
             ret, frame = video_cap.read()
+
             if ret == True:
+
+                #Preprocesses frame and adds name to list
                 pil_image = self.preprocess_frame(frame, self.x_begin, self.x_end, self.y_begin, self.y_end)
                 print(f'Reading frame: {count}')
                 count += 1
-                video_name = "test1"
-                image_names.append(self.save_frame_as_image(pil_image, count, video_name))
+                image_names.append(self.save_frame_as_image(pil_image, count))
+
             else:
                 break
 
         video_cap.release()
 
-        print('Starting OCR recognition')
+        #Recognize number in each frame and adds it to list
         value_per_frame = self.recognize_numbers(image_names)
+
+        #todo: might need to make these data members so they can be accessed
 
         average_value = np.mean(value_per_frame)
         min_value = min(value_per_frame)
@@ -80,9 +126,13 @@ class OcrService:
         plt.plot(value_per_frame)
         plt.show()
 
-        #todo: delete all the created images
-
         print(value_per_frame)
         print(f'average = {average_value}, min = {min_value}, max = {max_value}')
 
         return min_value, max_value, average_value
+    
+    def __del__(self):
+
+        #todo: delete video from directory and delete saved frames from directory
+
+        pass
