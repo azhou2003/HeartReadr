9cf9d64fddac3cdbc2d3c60936a0dcd0b2290198
Anjie Zhou <anjie.zhou2003@gmail.com>
2023-08-17 23:57:25 -0500

    added the favicon

--- HeartReadrSite/HeartReadrSite/urls.py
+++ HeartReadrSite/HeartReadrSite/urls.py
@@ -27,7 +27,4 @@ urlpatterns = [
     path('results/', results, name = 'results'),
 ]
 
-'''
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-'''
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
